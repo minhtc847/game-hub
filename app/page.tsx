c0982ba0ca02e6bc29e59be09804ab6229f6fcd0
minhtc30 <minhtc30@fpt.com>
2024-09-02 22:24:23 +0700

    Install Chakra UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import {Button} from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <main className="">
-
+      <Button colorScheme="teal">Button</Button>
     </main>
   );
 }
